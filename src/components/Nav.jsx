815149f6ad84176683f0fe2a8495bf0eff026a1e
mostafa3221 <mostafaabdrdo@gmail.com>
2025-08-07 02:26:54 +0300

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
 const Nav = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">
+        <Link to="/" className="nav-logo" onClick={closeMobileMenu}>
           UserDash
         </Link>
-        <ul className="nav-menu">
+
+        <div className={`menu-icon ${click ? 'is-active' : ''}`} onClick={handleClick}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/About" className="nav-links">
+            <Link to="/About" className="nav-links" onClick={closeMobileMenu}>
               About
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/Contact" className="nav-links">
+            <Link to="/Contact" className="nav-links" onClick={closeMobileMenu}>
               Contact
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/User" className="nav-links">
+            <Link to="/User" className="nav-links" onClick={closeMobileMenu}>
               Users
             </Link>
           </li>
